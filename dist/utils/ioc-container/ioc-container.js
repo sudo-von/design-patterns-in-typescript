8db6df320e4d521bbd78b401fa802c817da30d30
VoN <sudo.von.contact@gmail.com>
2022-01-03 23:27:47 -0600

    feat: :sparkles: Add ioc

---
+++ dist/utils/ioc-container/ioc-container.js
@@ -0,0 +1,49 @@
+"use strict";
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || Array.prototype.slice.call(from));
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.IoCContainer = void 0;
+var IoCContainer = /** @class */ (function () {
+    function IoCContainer() {
+        this._dependencies = {};
+        if (IoCContainer._instance) {
+            throw new Error("Singleton class. Cannot instantiate using new.");
+        }
+        IoCContainer._instance = this;
+    }
+    Object.defineProperty(IoCContainer, "instance", {
+        get: function () {
+            return IoCContainer._instance;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    IoCContainer.prototype.register = function (name, dependencies, implementation) {
+        if (this._dependencies[name]) {
+            throw new Error("Dependency already registered.");
+        }
+        var dependenciesImplementations = this.getDependenciesImplementations(dependencies);
+        this._dependencies[name] = new (implementation.bind.apply(implementation, __spreadArray([void 0], dependenciesImplementations, false)))();
+    };
+    IoCContainer.prototype.resolve = function (name) {
+        if (!this._dependencies[name]) {
+            throw new Error("Unresolved dependency ".concat(name, "."));
+        }
+        return this._dependencies[name];
+    };
+    IoCContainer.prototype.getDependenciesImplementations = function (names) {
+        var _this = this;
+        return names.map(function (name) { return _this.resolve(name); });
+    };
+    IoCContainer._instance = new IoCContainer();
+    return IoCContainer;
+}());
+exports.IoCContainer = IoCContainer;
+//# sourceMappingURL=ioc-container.js.map
---
+++ theory/utils/02-ioc-container.md
@@ -0,0 +1,12 @@
+#### What is reflection?
+
+* The ability to make use of code metadata to provide runtime information and inspection data about classes, interfaces and types.
+
+#### Reflection operations
+
+* Get the name of a method.
+* Know the types of a method's arguments list.
+* Retrieve assembly information.
+* Allow dependency injection to work.
+
+Use string keys instead of interfaces... because... JS...
